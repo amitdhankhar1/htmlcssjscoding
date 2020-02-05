fbdd83b2a3a971f64af49988e4ba9855d2c39da0
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-05 22:06:05 +0530

    add arry method

--- array.js
+++ array.js
@@ -1,3 +1,45 @@
-var arry=[3,2,4,1,5,6,];
+/*var arry=[3,2,4,1,5,6,];
 arry[arry.length]=7;
-console.log(arry);
+console.log(arry);*/
+
+function arrpush_small() {
+    var arr1 = [];
+    for (a = 0; a < 100; a++)
+    {
+        arr1 = [];
+        for (i = 0; i < 5000; i++)
+        {
+            arr1.push('elem' + i);
+        }
+    }
+}
+
+function arrlen_small() {
+    var arr2 = [];
+    for (b = 0; b < 100; b++)
+    {
+        arr2 = [];
+        for (j = 0; j < 5000; j++)
+        {
+            arr2[arr2.length] = 'elem' + j;
+        }
+    }
+}
+
+
+function arrpush_large() {
+    var arr1 = [];
+    for (i = 0; i < 50; i++)
+    {
+        arr1.push('elem' + i);
+        console.log(i);
+    }
+}
+
+function arrlen_large() {
+    var arr2 = [];
+    for (j = 0; j < 500000; j++)
+    {
+        arr2[arr2.length] = 'elem' + j;
+    }
+}
