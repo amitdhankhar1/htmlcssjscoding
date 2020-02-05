cea41a6e919bb60503c8bffc696b9d0b66f3d6d8
amit dhankhar <amitdhankhar616@gmail.com>
2020-02-05 22:30:09 +0530

    method

--- string.js
+++ string.js
@@ -1,7 +1,9 @@
-var str = "Apple, Banana, Kiwi";
+/*var str = "Apple, Banana, Kiwi";
 var res = str.slice(-12, -6);
 
 var str = "Please locat where locate occurs!";
 var res = str.search("locate");
+console.log(res);*/
+var str = "Please locate where 'locate' occurs!";
+var res = str.lastIndexOf("John");
 console.log(res);
-
